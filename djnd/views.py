1ef88d09cff4d160c3431e1cbfd1ebaa025e7bc4
LepkoQQ <lepko.san@gmail.com>
2019-07-01 13:46:47 +0200

    add tag filtering to project and video urls, return available tags in api response

--- djnd/views.py
+++ djnd/views.py
@@ -17,6 +17,19 @@ class TagsFilter(Filter):
         kwargs.setdefault('lookup_expr', 'in')
         super().__init__(*args, **kwargs)
 
+class VideoFilterSet(FilterSet):
+    tags = TagsFilter(field_name='tags__name')
+
+    class Meta:
+        model = Video
+        fields = ()  # add model fields here
+
+class ProjectFilterSet(FilterSet):
+    tags = TagsFilter(field_name='tags__name')
+
+    class Meta:
+        model = Project
+        fields = ()  # add model fields here
 
 class ClipFilterSet(FilterSet):
     tags = TagsFilter(field_name='tags__name')
@@ -34,16 +47,36 @@ class ClipFilterSet(FilterSet):
 class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
-    filter_backends = (filters.OrderingFilter,)
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
+    filterset_class = VideoFilterSet
     ordering_fields = ('order', 'date',)
     ordering = ('order',)
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        response.data.update({
+            'filters': {
+                'tags': list(Video.tags.most_common().values_list('name', flat=True).order_by('id')),
+            }
+        })
+        return response
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    filter_backends = (filters.OrderingFilter,)
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
+    filterset_class = ProjectFilterSet
     ordering_fields = ('order', 'date',)
     ordering = ('order',)
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        response.data.update({
+            'filters': {
+                'tags': list(Project.tags.most_common().values_list('name', flat=True).order_by('id')),
+            }
+        })
+        return response
 
 class ClipViewSet(viewsets.ModelViewSet):
     queryset = Clip.objects.all()
@@ -52,6 +85,18 @@ class ClipViewSet(viewsets.ModelViewSet):
     filterset_class = ClipFilterSet
     ordering_fields = ('order', 'date',)
     ordering = ('order',)
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        response.data.update({
+            'filters': {
+                'tags': list(Clip.tags.most_common().values_list('name', flat=True).order_by('id')),
+                'languages': list(Clip.languages.most_common().values_list('name', flat=True).order_by('id')),
+                'formats': list(Clip.formats.most_common().values_list('name', flat=True).order_by('id')),
+                'types': list(Clip.types.most_common().values_list('name', flat=True).order_by('id')),
+            }
+        })
+        return response
 
 class InfoPushViewSet(viewsets.ModelViewSet):
     queryset = InfoPush.objects.all()
