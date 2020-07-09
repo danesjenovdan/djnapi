94abe80d54231b823524726140917245d6ba0e89
LepkoQQ <lepko.san@gmail.com>
2020-07-09 23:00:35 +0200

    add some verbose names

--- djnd/models.py
+++ djnd/models.py
@@ -55,43 +55,43 @@ class TaggedTypeItem(GenericTaggedItemBase):
 # Create your models here.
 class Video(Translatable):
   title = models.CharField(max_length=512)
-  desc = models.TextField(blank=True, null=True)
-  url = models.URLField(blank=True, null=True)
+  desc = models.TextField(blank=True, null=True, verbose_name='Description')
+  url = models.URLField(blank=True, null=True, verbose_name='URL')
   date = models.DateField(blank=True, null=True)
   order = models.IntegerField(default=0, blank=False, null=False)
   image = models.FileField(upload_to='images/video/', blank=True)
   tags = TaggableManager(through=TaggedGenericItem, blank=True)
-  
+
   class TranslatableMeta:
     fields = ['title', 'desc']
-  
+
   class Meta:
     ordering = ['order']
-  
+
   def __str__(self):
     return self.title
 
 class Project(Translatable):
   title = models.CharField(max_length=512)
-  desc = models.TextField(blank=True, null=True)
+  desc = models.TextField(blank=True, null=True, verbose_name='Description')
   date = models.DateField(blank=True, null=True)
   order = models.IntegerField(default=0, blank=False, null=False)
-  url = models.URLField(blank=True, null=True)
+  url = models.URLField(blank=True, null=True, verbose_name='URL')
   image = models.FileField(upload_to='images/project/', blank=True)
   tags = TaggableManager(through=TaggedGenericItem, blank=True)
-  
+
   class TranslatableMeta:
     fields = ['title', 'desc']
-  
+
   class Meta:
     ordering = ['order']
-  
+
   def __str__(self):
     return self.title
 
 class Clip(Translatable):
   title = models.CharField(max_length=512)
-  desc = models.TextField(blank=True, null=True)
+  desc = models.TextField(blank=True, null=True, verbose_name='Description')
   date = models.DateField(blank=True, null=True)
   order = models.IntegerField(default=0, blank=False, null=False)
   publisher = models.CharField(max_length=512)
@@ -100,40 +100,44 @@ class Clip(Translatable):
   languages = TaggableManager(through=TaggedLanguageItem, blank=True, verbose_name='Language tags')
   formats = TaggableManager(through=TaggedFormatItem, blank=True, verbose_name='Format tags')
   types = TaggableManager(through=TaggedTypeItem, blank=True, verbose_name='Type tags')
-  url = models.URLField(blank=True, null=True)
-  
+  url = models.URLField(blank=True, null=True, verbose_name='URL')
+
   class TranslatableMeta:
     fields = ['title', 'desc']
-  
+
   class Meta:
     ordering = ['order']
-  
+
   def __str__(self):
     return self.title
 
 # this is a singleton
 class InfoPush(Translatable):
   title = models.CharField(max_length=512)
-  desc = models.TextField(blank=True, null=True)
-  cta_text = models.CharField(max_length=512)
-  cta_url = models.URLField()
+  desc = models.TextField(blank=True, null=True, verbose_name='Description')
+  cta_text = models.CharField(max_length=512, verbose_name='Button text')
+  cta_url = models.URLField(verbose_name='Button URL')
   visible = models.BooleanField(default=False)
   image = models.FileField(upload_to='images/infopush/', blank=True)
-  
+
   class TranslatableMeta:
     fields = ['title', 'desc', 'cta_text', 'cta_url']
+
+  class Meta:
+    verbose_name = 'Info push'
+    verbose_name_plural = 'Info pushes'
 
   def save(self, *args, **kwargs):
     self.pk = 1
     super(InfoPush, self).save(*args, **kwargs)
-  
+
   def delete():
     pass
-  
+
   @classmethod
   def load(cls):
     obj, created = cls.objects.get_or_create(pk=1)
     return obj
-  
+
   def __str__(self):
     return self.title
